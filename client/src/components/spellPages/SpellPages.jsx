d15796ed53337b94815d17b79843e8b241c8e5f0
Giulia Martins <116525917+giuliacmartins@users.noreply.github.com>
2023-10-31 21:24:36 -0400

    implemented image layout

--- client/src/components/spellPages/SpellPages.jsx
+++ client/src/components/spellPages/SpellPages.jsx
@@ -1,22 +1,43 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import "./assets/spellPagesStyle.css"
+import spellCartImage from "./assets/photos/spellcart.png";
+import searchImage from "./assets/photos/search.png";
+import forwardArrowImage from "./assets/photos/forwardArrow.png";
+import backArrowImage from "./assets/photos/backArrow.png";
+import arcanHeaderImage from "./assets/photos/arcane1LevelSpellsHeader.png";
+import buffBtnImage from "./assets/photos/buffBtn.png";
+import damageBtnImage from "./assets/photos/damageBtn.png";
+import tocBtnImage from "./assets/photos/tocBtn.png";
+import dropDownBtnImage from "./assets/photos/dropDownBtn.png";
+import homeBrownImage from "./assets/photos/homeBrown.png";
 
 function SpellPages(){
     let navigate = useNavigate();
     return(
         <div className="spellPage">
-            <div>
-                Hello! This is the Spell Pages!
+            <div className="navbar">
+                <img src={searchImage} alt="Search" onClick={() => {/* Navigate to search page */}}/>
+                <img className="spellCart" src={spellCartImage} alt="Spell Cart" onClick={() => {/* Navigate to cart page */}}/>
+                <img className="rightNav" src={homeBrownImage} alt="Home" onClick={() => navigate("/")}/>
+                <img className="rightNav" src={tocBtnImage} alt="TOC" onClick={() => navigate("/tableofContents")}/>
+                <img className="rightNav" src={damageBtnImage} alt="Damage" onClick={() => {/* Navigate to search page */}}/>
+                <img className="rightNav" src={buffBtnImage} alt="Buff" onClick={() => {/* Navigate to search page */}}/>
+                <img className="rightNav" src={dropDownBtnImage} alt="Drop Down" onClick={() => {/* Navigate to search page */}}/>
+            </div>
+
+            <div className="pagination">
+                <img src={backArrowImage} alt="Previous" onClick={() => navigate("/")}/>
+                <img src={forwardArrowImage} alt="Next" onClick={() => navigate("/tableofContents2")}/>
             </div>
             
-            <button onClick={() => {navigate("/")}}>
+            {/* <button onClick={() => {navigate("/")}}>
                 Go to Home!
             </button>
 
             <button onClick={() => {navigate("/tableOfContents")}}>
                 Go to Table of Contents!
-            </button>
+            </button> */}
         </div>
     )
 }
